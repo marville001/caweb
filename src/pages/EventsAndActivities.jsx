0549ffab56bbd0886540e408bf49f4d1fac3e4f9
marville001 <mwangimartin1904@gmail.com>
2021-07-21 21:40:11 +0300

    Events and Activities

--- src/pages/EventsAndActivities.jsx
+++ src/pages/EventsAndActivities.jsx
@@ -12,6 +12,64 @@ const EventsAndActivities = () => {
         title="Events and Activities"
         subtitle="upcoming"
       />
+      <div className="container events-container">
+      <div className="event">
+            <div className="event-left">
+              <img src={eventsBg} alt="Event Placeholder" />
+              <div className="event-left-date">
+                <span>May</span>
+                <span>21</span>
+              </div>
+            </div>
+            <div className="event-right">
+              <h2>Name</h2>
+              <h3>May 13, 12:00pm - May 13, 4:00pm</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, enim voluptates eveniet nostrum quisquam, perferendis.</p>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-left">
+              <img src={eventsBg} alt="Event Placeholder" />
+              <div className="event-left-date">
+                <span>May</span>
+                <span>21</span>
+              </div>
+            </div>
+            <div className="event-right">
+              <h2>Name</h2>
+              <h3>May 13, 12:00pm - May 13, 4:00pm</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, enim voluptates eveniet nostrum quisquam, perferendis.</p>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-left">
+              <img src={eventsBg} alt="Event Placeholder" />
+              <div className="event-left-date">
+                <span>May</span>
+                <span>21</span>
+              </div>
+            </div>
+            <div className="event-right">
+              <h2>Name</h2>
+              <h3>May 13, 12:00pm - May 13, 4:00pm</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, enim voluptates eveniet nostrum quisquam, perferendis.</p>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-left">
+              <img src={eventsBg} alt="Event Placeholder" />
+              <div className="event-left-date">
+                <span>May</span>
+                <span>21</span>
+              </div>
+            </div>
+            <div className="event-right">
+              <h2>Name</h2>
+              <h3>May 13, 12:00pm - May 13, 4:00pm</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, enim voluptates eveniet nostrum quisquam, perferendis.</p>
+            </div>
+          </div>
+      </div>
     </div>
   );
 };
