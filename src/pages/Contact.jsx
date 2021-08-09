bea0c359f76b96beb6ef0bb3c8cf9b7063ac2857
marville001 <mwangimartin1904@gmail.com>
2021-08-09 16:28:57 +0300

    Added contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,47 @@ import React from "react";
 import PageHeader from "../components/PageHeader";
 import eventsBg from "../images/events-header-bg.jpg";
 
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+
+import "../styles/contact.scss";
+
 const Contact = () => {
   return (
     <div>
       <PageHeader image={eventsBg} title="Contact Us" subtitle="Get in Touch" />{" "}
+      <div className="contact-container">
+        <div className="content container">
+          <TextField
+            label="Name"
+            type="text"
+            variant="outlined"
+            className="input"
+          />
+          <TextField
+            label="Email"
+            type="email"
+            variant="outlined"
+            className="input"
+          />
+          <TextField
+            label="Subject"
+            type="text"
+            variant="outlined"
+            className="input"
+          />
+          <TextareaAutosize
+            rowsMin={4}
+            placeholder="Message"
+            className="input textarea"
+          />
+          <Button variant="contained" color="primary">
+            Send Message
+          </Button>
+
+        </div>
+      </div>
     </div>
   );
 };
