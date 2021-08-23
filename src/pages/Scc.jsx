4e5e83586b166b47bd987419205fb3645163f90c
marville001 <mwangimartin1904@gmail.com>
2021-08-23 16:54:13 +0300

    Updated scc images

--- src/pages/Scc.jsx
+++ src/pages/Scc.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import "../styles/scc.scss";
 import PageHeader from "../components/PageHeader";
 import newsBg from "../images/news-header-bg.jfif";
+import stPetersImg from "../images/stpeters.jpg";
+import stJosephImg from "../images/stjoseph.jpg";
+import stAngelusImg from "../images/stangelus.jpg";
+
+import stJohnImg from "../images/stjohn.jpg";
+import stVitusImg from "../images/stvitus.jpg";
+import stGabrielImg from "../images/stgabriel.jpg";
+import choirImg from "../images/choir.jpg";
+import ladiesImg from "../images/ladies.jpg";
+import gentsImg from "../images/gents.jpg";
+import SccCard from "../components/SccCard";
 
 const Scc = () => {
   return (
@@ -46,20 +57,27 @@ const Scc = () => {
           </div>
         </div>
 
-        
         {/*Major SCC GROUPS */}
         <div className="scc-groups">
           <div className="container">
             <h2>MAJOR SCC GROUPS</h2>
 
             <div className="scc-groups-container">
-              {[1,2,3].map(group=>(
-              <div className="scc-group">
-                <h3>Saint Joseph SCC</h3>
-                <img src={newsBg} alt={"Saint Joseph Scc"} />
-                <p>For all students belonging to the school of Engineering, Geospatial and Geomatics</p>
-              </div>
-              ))}
+              <SccCard
+                title="Saint Angela SCC"
+                image={stAngelusImg}
+                description="For all students belonging to Computer science, Information Technology(I.T) ,Industrial Chemistry, Leather Technology and School Of Health Science. "
+              />
+              <SccCard
+                title="Saint Joseph SCC"
+                image={stJosephImg}
+                description="For all students belonging to the school of Engineering, Geospatial and geomatics. "
+              />
+              <SccCard
+                title="Saint Peter SCC"
+                image={stPetersImg}
+                description="For all students pertaking business related courses."
+              />
             </div>
           </div>
         </div>
@@ -70,13 +88,37 @@ const Scc = () => {
             <h2>OTHER SCC GROUPS</h2>
 
             <div className="scc-groups-container">
-              {[1,2,3,4,5,6].map(group=>(
-              <div className="scc-group">
-                <h3>Saint Joseph SCC</h3>
-                <img src={newsBg} alt={"Saint Joseph Scc"} />
-                <p>For all students belonging to the school of Engineering, Geospatial and Geomatics</p>
-              </div>
-              ))}
+            <SccCard
+                title="Saint Cecilia Choir"
+                image={choirImg}
+                description="This absorbs students from the all schools within the institution with the main objective being to sing during the mass. the Choir has also recorded various songs. the participation goes beyond church service as they also pertake in various events within and outside the university."
+              />
+              <SccCard
+                title="Saint Vitus Innovetia"
+                image={stVitusImg}
+                description="This also absorbs students from the all schools within the institution with the main objective being to spread the gospel through talent. They perform various activities ranging from dancing, art, singing and many more. they also lead the liturgical team during mass."
+              />
+              <SccCard
+                title="Saint John Altarservers"
+                image={stJohnImg}
+                description="This team entails all the altar boys and girls whose main objective is to prepare and serve mass. they also organise sacrament of confession when the priest is to come. They also lead the Christians through the Catholic Doctrines."
+              />
+              <SccCard
+                title="Saint Gabriel"
+                image={stGabrielImg}
+                description="This team entails the instrument team. They are in charge of controlling Adoration and maintaining the church Instrument systems including th sound systems, music instruments projectors. they Take charge of the instruments especially during the Holy Mass and during Koinonea
+                They are also incharge of all the communication media of the Church."
+              />
+              <SccCard
+                title="Saint Paul CA Gents"
+                image={gentsImg}
+                description="All gentlemen by default belong to this group."
+              />
+              <SccCard
+                title="Saint Catherine of Siena CA Ladies"
+                image={ladiesImg}
+                description="All ladies by default belong to this group."
+              />
             </div>
           </div>
         </div>
