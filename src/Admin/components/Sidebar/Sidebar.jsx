a12a83e22feb670284d882ad48d2f66382e8d4ab
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-02 14:32:08 +0300

    Added sidebar items and logout button

--- src/Admin/components/Sidebar/Sidebar.jsx
+++ src/Admin/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,50 @@
 import React, { useState } from "react";
 import { Box, Button, styled } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { Home, PeopleAlt, Event, Notifications,Analytics, Message } from "@mui/icons-material";
+
+const dashitems = [
+  { to: "/admin/", label: "Home", icon: <Home /> },
+  { to: "/admin/users", label: "Users", icon: <PeopleAlt /> },
+  { to: "/admin/events", label: "Events", icon: <Event /> },
+];
+
+const genitems = [
+  { to: "/admin/", label: "Analytics", icon: <Analytics /> },
+  { to: "/admin/users", label: "Notifications", icon: <Notifications /> },
+  { to: "/admin/events", label: "Messages", icon: <Message /> },
+];
+
+const Sidebar = ({ open }) => {
+  return (
+    <SidebarContainer open={open}>
+      <div>
+        <SidebarHeader>
+          <h3>Welcome</h3>
+        </SidebarHeader>
+        <SidebarMenuItem items={dashitems} title="Dashboard" />
+        <SidebarMenuItem items={genitems} title="General" />
+        <SidebarMenuItem />
+        <SidebarMenuItem />
+        <SidebarMenuItem />
+
+        <LogoutButton>
+          <Button style={{flex: 1}} variant="contained">Logout</Button>
+        </LogoutButton>
+      </div>
+    </SidebarContainer>
+  );
+};
 
 const SidebarContainer = styled(Box)(({ theme, open }) => ({
   background: "#fff",
+  position: "sticky",
+  top: "0px",
+  left: "0px",
+  height: "100vh",
+  overflow: "auto",
   width: open ? "280px" : "0px",
-  borderRight: "1px solid #f5f5f1",
-  padding: open ? "20px" : "0px",
+  borderRight: "2px solid #f5f5f1",
   transition: "all .3s linear",
   [theme.breakpoints.down("md")]: {
     transform: open ? "translateX(0)" : "translateX(-100%)",
@@ -18,22 +56,20 @@ const SidebarContainer = styled(Box)(({ theme, open }) => ({
     opacity: 1,
     zIndex: 100,
     width: "280px",
-    padding: "20px",
   },
 }));
 
-const Sidebar = ({ open }) => {
-  return (
-    <SidebarContainer open={open}>
-      <div>
-        <h1 className="side-title">My Title</h1>
-        <ul>
-          <li>Dashboard</li>
-          <NavLink to="/admin/"></NavLink>
-        </ul>
-      </div>
-    </SidebarContainer>
-  );
-};
+const SidebarHeader = styled(Box)(({ theme, open }) => ({
+  height:"100px",
+  // textAlign:"center",
+  padding:"30px",
+  // backgroundColor:theme.palette.primary.light
+}))
+
+const LogoutButton = styled(Box)(({ theme, open }) => ({
+  display:"flex",
+  justifyContent:"center",
+  padding:"20px 40px"
+}))
 
 export default Sidebar;
---
+++ src/Admin/components/Sidebar/SidebarMenuItem.jsx
@@ -0,0 +1,63 @@
+import { IconButton, styled } from "@mui/material";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SidebarMenuItem = ({ title, items, icon: Icon }) => {
+  return (
+    <div>
+      <ListTitle>{title}</ListTitle>
+      <ListContainer>
+        {items?.map(({ to, icon, label }, i) => (
+          <ListItem key={i} activeClassName="nav-active" to={to}>
+            <ListIcon>{icon}</ListIcon>
+            {label}
+          </ListItem>
+        ))}
+      </ListContainer>
+    </div>
+  );
+};
+
+const ListContainer = styled("div")(({ theme }) => ({
+  // background:"yellow"
+  margin: "0px",
+}));
+
+const ListTitle = styled("h4")(({ theme }) => ({
+  fontSize: "22px",
+  margin: "5px 30px",
+  color: "rgba(0,0,0,0.6)",
+}));
+
+const ListItem = styled(NavLink)(({ theme }) => ({
+  display: "block",
+  padding: "12px",
+  paddingLeft: "40px",
+  fontSize: "18px",
+  color: "#A8B1B9",
+  margin: "0px",
+  display: "flex",
+  alignItems: "center",
+  "&:hover": {
+    color: theme.palette.primary.main,
+    background: theme.palette.primary.light,
+    borderRight: "5px solid",
+    borderRightColor: theme.palette.primary.main,
+    '.MuiIconButton-root':{
+        color: theme.palette.primary.main,
+    }
+  },
+  "&:active": {
+    color: theme.palette.primary.main,
+    background: theme.palette.primary.light,
+    borderRight: "5px solid",
+    borderRightColor: theme.palette.primary.main,
+  },
+}));
+
+const ListIcon = styled(IconButton)(({ theme }) => ({
+  fontSize: "16px",
+  color: "#A8B1B9",
+}));
+
+export default SidebarMenuItem;
