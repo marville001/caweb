5233410ad6ffcba93b3e64855cf9339f11a0e588
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-03 20:34:59 +0300

    updates

--- src/Admin/components/custom/PageHeaderWithBreadCamp.jsx
+++ src/Admin/components/custom/PageHeaderWithBreadCamp.jsx
@@ -5,19 +5,20 @@ import Link from "@mui/material/Link";
 import { styled } from "@mui/material/styles";
 import { Button } from "@mui/material";
 
-import { useHistory } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
 const PageHeaderWithBreadCamp = ({ title }) => {
   const history = useHistory();
 
   const generateCrumb = () => {
     const paths = history.location.pathname.split("/");
-    if(paths[paths.length - 1] === ""){
+    if (paths[paths.length - 1] === "") {
       paths.pop();
     }
+
     paths.shift();
 
-    console.log({paths});
+    console.log({ paths });
 
     if (paths.length === 1) {
       return [
@@ -25,27 +26,38 @@ const PageHeaderWithBreadCamp = ({ title }) => {
           Dashboard
         </Typography>,
       ];
-    }else {
-      return [
-        <Link underline="hover" key="1" color="inherit" href="/">
-          MUI
-        </Link>,
-        <Link
-          underline="hover"
-          key="2"
-          color="inherit"
-          href="/getting-started/installation/"
-        >
-          Core
-        </Link>,
+    } else {
+      const crumbs = [
+        <NavLink underline="hover" style={{ color: "gray" }} to="/admin">
+          Dashboard
+        </NavLink>,
+      ];
+
+      const last = paths[paths.length - 1]
+
+      if (paths.length > 2) {
+        paths.pop();
+        paths.unshift();
+
+        for (let p of paths) {
+          crumbs.push(
+            <NavLink underline="hover" style={{ color: "gray" }} to="/admin/">
+              {p}
+            </NavLink>
+          );
+        }
+      }
+
+      crumbs.push(
         <Typography key="3" color="text.primary">
-          Breadcrumb
-        </Typography>,
-      ]
+          {last}
+        </Typography>
+      );
+      return crumbs;
     }
   };
   generateCrumb();
-  
+
   return (
     <CompContainer>
       <div>
