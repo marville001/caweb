b1a4c7856c27d0728abf16f7e70a61a813719fee
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-04 08:22:58 +0300

    Updated home dashboard

--- src/Admin/pages/HomeDashboard/HomeDashboard.jsx
+++ src/Admin/pages/HomeDashboard/HomeDashboard.jsx
@@ -1,3 +1,5 @@
+import { Grid, Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import PageHeaderWithBreadCamp from "../../components/custom/PageHeaderWithBreadCamp";
@@ -12,8 +14,53 @@ const HomeDashboard = () => {
   return (
     <MainContainer>
       <PageHeaderWithBreadCamp noaction title="Dashboard" crumbs={crumbs} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <TopCard>hrrh</TopCard>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <TopCard>hrrh</TopCard>
+        </Grid>
+      </Grid>
+      <div style={{ margin: "30px 0px" }} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <DashCard>Card</DashCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <DashCard>Card</DashCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <DashCard>Card</DashCard>
+        </Grid>
+      </Grid>
+      <div style={{ margin: "30px 0px" }} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <GraphCard>Graph One</GraphCard>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <GraphCard>Graph Two</GraphCard>
+        </Grid>
+      </Grid>
     </MainContainer>
   );
 };
+
+const TopCard = styled(Paper)(({ theme }) => ({
+  padding: "20px",
+  marginTop: "20px",
+  minHeight: "150px",
+}));
+
+const DashCard = styled(Paper)(({ theme }) => ({
+  padding: "20px",
+  minHeight: "120px",
+}));
+
+const GraphCard = styled(Paper)(({ theme }) => ({
+  padding: "20px",
+  minHeight: "200px",
+}));
 
 export default HomeDashboard;
