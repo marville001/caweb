e7db1100956fb46807a316f5045ede960f78e961
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-05 16:14:33 +0300

    Added events table

--- src/pages/EventsAndActivities.jsx
+++ src/pages/EventsAndActivities.jsx
@@ -1,27 +1,58 @@
 import React from "react";
 import "../styles/events.scss";
-
-import eventsBg from "../images/activities-header-bg.jfif";
-import PageHeader from "../components/PageHeader";
-import EventCard from "../components/EventCard";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import { tableCellClasses } from '@mui/material/TableCell';
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.common.white,
+    fontSize:"16px"
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 12,
+  },
+}));
 
 const EventsAndActivities = () => {
   return (
     <div className="events-containers">
-      <PageHeader
-        image={eventsBg}
-        title="Events and Activities"
-        subtitle="upcoming"
-      />
-      <div className="container events-container">
-          {[1,2,3,4,5,6].map(item=>
-          <EventCard
-            image={eventsBg}
-            day="16"
-            month="Jul"
-            name="Charity Activity Mweiga"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, enim voluptates eveniet nostrum quisquam, perferendis."
-          />)}
+      <div className="container eventss-container">
+        <h2 style={{ margin: "30px", textAlign: "center" }}>
+          Weekly Activities
+        </h2>
+        <TableContainer component={Paper}>
+          <Table sx={{ width: "100%", minWidth:"500px" }}>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>#</StyledTableCell>
+                <StyledTableCell>Activity</StyledTableCell>
+                <StyledTableCell>Day</StyledTableCell>
+                <StyledTableCell>Time</StyledTableCell>
+                <StyledTableCell>Venue</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((e) => (
+                <TableRow key={e}>
+                  <StyledTableCell>{e}</StyledTableCell>
+                  <StyledTableCell>jhhddjh</StyledTableCell>
+                  <StyledTableCell>jhhddjh</StyledTableCell>
+                  <StyledTableCell>jhhddjh</StyledTableCell>
+                  <StyledTableCell>jhhddjh</StyledTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
